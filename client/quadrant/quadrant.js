814fa5e88666dcfada8f193f30490d8232ab07e9
forrestbthomas <forrestbthomas@gmail.com>
2014-09-02 20:24:09 -0700

    Initial Commit

---
+++ client/quadrant/quadrant.js
@@ -0,0 +1,13 @@
+angular.module('myApp.main.quadrant', ['ui.router'])
+
+.config(function ($stateProvider) {
+
+  $stateProvider
+    .state('myApp.main.quadrants', {
+      url: '/quadrants',
+      templateUrl: 'quadrant/quadrant.tpl.html',
+      controller: 'QuadrantController'
+    });
+})
+.controller('QuadrantController', function ($scope) {
+});
